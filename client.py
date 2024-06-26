a6f7e21708fb9705ed418cb28d57ef724b7ffe35
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-06-26 11:09:34 +0200

    first correct impl of chat

--- client.py
+++ client.py
@@ -1,1 +1,80 @@
-# This is the client manager
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+import tkinter as tkt
+
+# Method to receive message from the chat
+def receive():
+    while True:
+        try:
+        
+            msg = client_socket.recv(BUFFSIZE).decode("utf8")
+            msg_list.insert(tkt.END, msg)
+        
+        except OSError:
+            break
+
+# Method to send message to the chat
+def send(event=None):
+    msg = my_msg.get()
+    my_msg.set("")
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        window.quit()
+
+# Action to perform when the window is closed
+def on_closing(event=None):
+    my_msg.set("{quit}")
+    send()
+
+# Window initialization
+window = tkt.Tk()
+window.title("Chat")
+
+message_frame = tkt.Frame(window)
+
+# Text field
+my_msg = tkt.StringVar()
+my_msg.set("Insert here your text")
+scrollbar = tkt.Scrollbar(message_frame)
+
+# Messages box
+msg_list = tkt.Listbox(message_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkt.RIGHT, fill=tkt.Y)
+msg_list.pack(side=tkt.LEFT, fill=tkt.BOTH)
+msg_list.pack()
+message_frame.pack()
+
+# Input field
+entry_field = tkt.Entry(window, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+
+# Send button
+send_button = tkt.Button(window, text="Send", command=send)
+send_button.pack()
+
+# When closing window
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+#HOST = input("Server host: ")
+#PORT = input("Server port: ")
+HOST = "127.0.0.1"
+PORT = 5300
+
+
+if not PORT:        # Invalid port
+    PORT = 5300
+else:
+    PORT = int(PORT)
+
+BUFFSIZE = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target = receive)
+receive_thread.start()
+
+tkt.mainloop()
--- server.py
+++ server.py
@@ -5,33 +5,36 @@ from threading import Thread
 
 def accept_connection():
     while True:
-        client, client_address = SERVER.accept()    # client is the socket we use to communicate
-        print("%s connected to chat" %client_address)
+        client, client_address = SERVER.accept()  # client is the socket we use to communicate
+        print("%s:%s connected to chat" % client_address)
         client.send(bytes("Hello, what's your name?","utf8"))
         addresses[client] = client_address
         
-        Thread(target = manage_client, args = (client)).start() # New thread for each connection
+        Thread(target = manage_client, args = (client,)).start() # New thread for each connection
 
 def manage_client(client):
     name = client.recv(BUFFSIZE).decode("utf8")
     clients[client] = name
-    welcome = "Welcome to the chat, %s . Write {quit} to quit the chat" %name
+    welcome = "Welcome to the chat, %s . Write {quit} to quit the chat" % name
     client.send(bytes(welcome, "utf8"))
+    
+    print(name, " joined.")
 
     while True:
         msg = client.recv(BUFFSIZE).decode("utf8")
         if msg != "{quit}":
-            formatted_msg = "[%s]: %s" %name %msg
+            formatted_msg = "[%s]: %s" % (name, msg)
+            print(formatted_msg)
             broadcast(bytes(formatted_msg, "utf8"))
         else:
             client.send(bytes("Goodbye!", "utf8"))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." %name, "utf8"))
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
             
 def broadcast(msg): #TODO: add name prefix here instead of in manage_client
     for client in clients:
-        client.send(bytes(msg, "utf8"))
+        client.send(bytes("%s" % msg, "utf8"))
 
 clients = {}
 addresses = {}
